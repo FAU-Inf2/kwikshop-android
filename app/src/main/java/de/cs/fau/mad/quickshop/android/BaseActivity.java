114f64e297e06d81a9d825bd4e3ba5da1c0f9316
Andreas Grünwald <andreasgruenwald@live.de>
2015-05-25 15:55:54 +0200

    Set frameLayout field in BaseActivity

--- app/src/main/java/de/cs/fau/mad/quickshop/android/BaseActivity.java
+++ app/src/main/java/de/cs/fau/mad/quickshop/android/BaseActivity.java
@@ -31,6 +31,8 @@ public class BaseActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        frameLayout = (FrameLayout) findViewById(R.id.content_frame);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
     }

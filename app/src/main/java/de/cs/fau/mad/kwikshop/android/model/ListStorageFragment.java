552d51ee209deff2a218e50a23e3c7231d619386
nash <Maxx1991@hotmail.de>
2015-06-11 22:45:59 +0200

    renamed db list and added example items with tutorial description

--- app/src/main/java/de/cs/fau/mad/kwikshop/android/model/ListStorageFragment.java
+++ app/src/main/java/de/cs/fau/mad/kwikshop/android/model/ListStorageFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentManager;
 
 import java.sql.SQLException;
 
+import cs.fau.mad.kwikshop_android.R;
 import de.cs.fau.mad.kwikshop.android.common.Group;
 import de.cs.fau.mad.kwikshop.android.common.Item;
 import de.cs.fau.mad.kwikshop.android.common.ShoppingList;
@@ -94,41 +95,46 @@ public class ListStorageFragment extends Fragment {
         if(m_LocalListStorage.getAllLists().size() == 0) {
             int m = m_LocalListStorage.createList();
             ShoppingList list = m_LocalListStorage.loadList(m);
-            list.setName("DB List 1");
+            list.setName(context.getString(R.string.descr_my_first_shopping_list));
+
 
             Item i1 = new Item();
-            i1.setName("DB Item 1");
-            i1.setComment("Sample comment");
+            i1.setName(context.getString(R.string.descr_apple));
+            i1.setComment(context.getString(R.string.descr_quickadd));
             list.addItem(i1);
 
-            Item i2 = new Item();
-            i2.setName("DB Item 2");
-            i2.setComment("Some really long comment. It just goes on and on and on and on and on and on and on and on and on and on and on and on");
-            i2.setBought(true);
-            list.addItem(i2);
-
-            Item i3 = new Item();
-            i3.setName("DB Item 3");
-            list.addItem(i3);
-
-            Item i4 = new Item();
-            i4.setName("DB Item 4");
-            i4.setComment("Comment");
-            i4.setBrand("Some brand name");
-            list.addItem(i4);
-
-
             Item i5 = new Item();
-            i5.setName("DB Item 4");
-            i5.setComment("Comment");
-            i5.setBrand("Some brand name");
-            i5.setAmount(250);
+            i5.setName(context.getString(R.string.descr_sweets));
+            i5.setBrand(context.getString(R.string.descr_drag_and_drop1));
+            i5.setComment(context.getString(R.string.descr_drag_and_drop2));
             list.addItem(i5);
 
             Item i6 = new Item();
-            i6.setName("DB Item 6");
+            i6.setName(context.getString(R.string.descr_coke));
+            i6.setBrand(context.getString(R.string.descr_swipe));
             i6.setAmount(5);
             list.addItem(i6);
+
+
+            Item i3 = new Item();
+            i3.setName(context.getString(R.string.descr_spaghettis));
+            i3.setBrand(context.getString(R.string.descr_fab1));
+            i3.setComment(context.getString(R.string.descr_fab2));
+            i3.setAmount(3);
+            list.addItem(i3);
+
+            Item i4 = new Item();
+            i4.setName(context.getString(R.string.descr_toilet_paper));
+            i4.setBrand(context.getString(R.string.descr_details1));
+            i4.setComment(context.getString(R.string.descr_details2));
+            i4.setHighlight(true);
+            list.addItem(i4);
+
+            Item i2 = new Item();
+            i2.setName(context.getString(R.string.descr_already_bought));
+            i2.setBought(true);
+            list.addItem(i2);
+
 
             list.save();
         }

1ffa0e69b0124103fe1f784c9cbb1d983d7863a2
Robert <robert.miller@fau.de>
2015-06-18 14:33:18 +0200

    display city name in location

--- app/src/main/java/de/fau/cs/mad/kwikshop/android/view/LocationFragment.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/view/LocationFragment.java
@@ -3,6 +3,8 @@ package de.fau.cs.mad.kwikshop.android.view;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -24,6 +26,10 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import de.fau.cs.mad.kwikshop.android.R;
@@ -34,10 +40,18 @@ public class LocationFragment extends Fragment implements GoogleApiClient.Connec
     // LogCat tag
     private static final String TAG = LocationActivity.class.getSimpleName();
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+
     @InjectView(R.id.tv_location)
     TextView tv_location;
+
+    @InjectView(R.id.tv_city)
+    TextView tv_city;
+
     private View rootView;
+
     private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+
 
     public static LocationFragment newInstance() {
 
@@ -113,7 +127,7 @@ public class LocationFragment extends Fragment implements GoogleApiClient.Connec
         return true;
     }
 
-    // Method to Check GPS is enable or disable
+    // Method to check  if GPS is enabled or disabled
     private void displayGpsStatus() {
         ContentResolver contentResolver = getActivity().getBaseContext().getContentResolver();
         boolean gpsStatus = Settings.Secure.isLocationProviderEnabled(contentResolver,
@@ -122,8 +136,9 @@ public class LocationFragment extends Fragment implements GoogleApiClient.Connec
                 Toast.LENGTH_LONG).show();
     }
 
+    // Method to display the coordinates
     private void displayLocation(){
-        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
         if (mLastLocation != null) {
             tv_location.setText("Latitude: " + String.valueOf(mLastLocation.getLatitude()) +
@@ -131,10 +146,30 @@ public class LocationFragment extends Fragment implements GoogleApiClient.Connec
         }
     }
 
+    // Method to display your city name
+    private void displayCityName(){
+
+        Geocoder gcd = new Geocoder(getActivity().getBaseContext(), Locale.getDefault());
+        List<Address> addresses;
+        try {
+            addresses = gcd.getFromLocation(mLastLocation.getLatitude(),  mLastLocation.getLongitude(), 1);
+            if (addresses.size() > 0){
+                System.out.println(addresses.get(0).getLocality());
+                String cityName = addresses.get(0).getLocality();
+                tv_city.setText(cityName);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 
     @Override
     public void onConnected(Bundle bundle) {
       displayLocation();
+      displayCityName();
     }
 
     @Override

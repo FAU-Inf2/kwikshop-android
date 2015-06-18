24d48cf4213860294f701ac9a0e0dbe9dc946bc0
Robert <robert.miller@fau.de>
2015-06-18 14:21:24 +0200

    setup location coordinates

--- app/src/main/java/de/fau/cs/mad/kwikshop/android/view/LocationFragment.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/view/LocationFragment.java
@@ -1,19 +1,25 @@
 package de.fau.cs.mad.kwikshop.android.view;
 
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
@@ -23,15 +29,13 @@ import butterknife.InjectView;
 import de.fau.cs.mad.kwikshop.android.R;
 
 
-
 public class LocationFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    @InjectView(R.id.tv_location_latidude)
-    TextView tv_location_latidude;
-
-    @InjectView(R.id.tv_location_longitude)
-    TextView tv_location_longitude;
-
+    // LogCat tag
+    private static final String TAG = LocationActivity.class.getSimpleName();
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+    @InjectView(R.id.tv_location)
+    TextView tv_location;
     private View rootView;
     private GoogleApiClient mGoogleApiClient;
 
@@ -47,10 +51,14 @@ public class LocationFragment extends Fragment implements GoogleApiClient.Connec
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        buildGoogleApiClient();
+        // First we need to check availability of play services
+        if (checkPlayServices()) {
+            buildGoogleApiClient();
+            displayGpsStatus();
+        }
+
     }
 
-    
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -58,10 +66,22 @@ public class LocationFragment extends Fragment implements GoogleApiClient.Connec
 
         rootView = inflater.inflate(R.layout.fragment_location, container, false);
         ButterKnife.inject(this, rootView);
-
         return rootView;
 
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        checkPlayServices();
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -73,15 +93,48 @@ public class LocationFragment extends Fragment implements GoogleApiClient.Connec
                 .build();
     }
 
-    @Override
-    public void onConnected(Bundle bundle) {
+    /**
+     * Method to verify google play services on the device
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity()
+                .getApplicationContext());
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, getActivity(),
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getActivity().getApplicationContext(), "This device is not supported.",
+                        Toast.LENGTH_LONG).show();
+                getActivity().finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    // Method to Check GPS is enable or disable
+    private void displayGpsStatus() {
+        ContentResolver contentResolver = getActivity().getBaseContext().getContentResolver();
+        boolean gpsStatus = Settings.Secure.isLocationProviderEnabled(contentResolver,
+                LocationManager.GPS_PROVIDER);
+        Toast.makeText(getActivity().getApplicationContext(), "GPS status: " + gpsStatus,
+                Toast.LENGTH_LONG).show();
+    }
+
+    private void displayLocation(){
         Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
         if (mLastLocation != null) {
-            tv_location_latidude.setText(String.valueOf(mLastLocation.getLatitude()));
-            tv_location_longitude.setText(String.valueOf(mLastLocation.getLongitude()));
+            tv_location.setText("Latitude: " + String.valueOf(mLastLocation.getLatitude()) +
+                    " Longitude: " + String.valueOf(mLastLocation.getLongitude()));
         }
+    }
 
+
+    @Override
+    public void onConnected(Bundle bundle) {
+      displayLocation();
     }
 
     @Override
@@ -91,7 +144,7 @@ public class LocationFragment extends Fragment implements GoogleApiClient.Connec
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
     }
 }
 

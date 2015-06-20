690dae0a93185eefc5d73d3f4b37baa6b42336d2
Robert <robert.miller@fau.de>
2015-06-21 00:25:40 +0200

    testing google place api

--- app/src/main/java/de/fau/cs/mad/kwikshop/android/view/LocationFragment.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/view/LocationFragment.java
@@ -1,7 +1,9 @@
 package de.fau.cs.mad.kwikshop.android.view;
 
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
@@ -10,10 +12,24 @@ import android.os.Bundle;
 
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -21,6 +37,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
@@ -31,12 +48,25 @@ import de.fau.cs.mad.kwikshop.android.model.InternetHelper;
 import de.fau.cs.mad.kwikshop.android.model.LocationFinder;
 
 
-public class LocationFragment extends Fragment implements  OnMapReadyCallback {
+public class LocationFragment extends Fragment implements  OnMapReadyCallback,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private View rootView;
     private GoogleMap map;
     private AlertDialog alert;
 
+    @InjectView(R.id.textView)
+    TextView placeName;
+
+
+    private static final String LOG_TAG = "PlacesAPIActivity";
+    private static final int GOOGLE_API_CLIENT_ID = 0;
+    private GoogleApiClient mGoogleApiClient;
+
+
+    private static final LatLngBounds BOUNDS_MOUNTAIN_VIEW = new LatLngBounds(
+            new LatLng(37.398160, -122.180831), new LatLng(37.430610, -121.972090));
+    private static final int PLACE_PICKER_REQUEST = 1;
 
     public static LocationFragment newInstance() {
         LocationFragment fragment = new LocationFragment();
@@ -54,11 +84,49 @@ public class LocationFragment extends Fragment implements  OnMapReadyCallback {
         super.onCreate(savedInstanceState);
         rootView = inflater.inflate(R.layout.fragment_location, container, false);
         ButterKnife.inject(this, rootView);
-        initiateMap();
+
+
+
+        /*
+        Context context = getActivity().getApplicationContext();
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(Places.PLACE_DETECTION_API)
+                .build();
+
+        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+                .getCurrentPlace(mGoogleApiClient, null);
+
+        result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+            @Override
+            public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                    Log.i(LOG_TAG, String.format("Place '%s' with " +
+                                    "likelihood: %g",
+                            placeLikelihood.getPlace().getName(),
+                            placeLikelihood.getLikelihood()));
+
+                }
+
+                likelyPlaces.release();
+            }
+        });
+        */
+
+
+        //initiateMap();
         return rootView;
 
     }
 
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
 
 
     private void initiateMap(){
@@ -128,6 +196,17 @@ public class LocationFragment extends Fragment implements  OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(LOG_TAG, "Google Places API connection failed with error code: "
+                + connectionResult.getErrorCode());
+
+        Toast.makeText(getActivity().getApplicationContext(),
+                "Google Places API connection failed with error code:" +
+                        connectionResult.getErrorCode(),
+                Toast.LENGTH_LONG).show();
     }
 }
 

28f5776db222392038c58ecffdee179879226c09
Robert <robert.miller@fau.de>
2015-06-22 23:06:47 +0200

    add network state change listener to InternetHelper

--- app/src/main/java/de/fau/cs/mad/kwikshop/android/model/InternetHelper.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/model/InternetHelper.java
@@ -2,11 +2,15 @@ package de.fau.cs.mad.kwikshop.android.model;
 
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
-public class InternetHelper {
+public class InternetHelper extends BroadcastReceiver {
+
+    NetworkStateChangeListener listener;
 
 
     public static boolean checkInternetConnection(Activity activity) {
@@ -14,6 +18,28 @@ public class InternetHelper {
                 (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if (info != null) {
+            if (info.isConnected()) {
+               listener.onNewNetworkConnection();
+            } else {
+                listener.onLostNetworkConnection();
+            }
+        }
+    }
+
+    public void setOnNetworkStateChangeListener(NetworkStateChangeListener listener){
+        this.listener = listener;
+    }
+
+    public interface NetworkStateChangeListener{
+        public void onLostNetworkConnection();
+        public void onNewNetworkConnection();
     }
 
 }

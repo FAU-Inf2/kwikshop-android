46933da608fe09dce370f739027d000e7ecd74cb
nash <Maxx1991@hotmail.de>
2015-06-24 18:23:38 +0200

    made some adjustments in ListOfShoppingListsActivity and ShoppingListDetailsViewModel, so id gets set correctly

--- app/src/main/java/de/fau/cs/mad/kwikshop/android/view/ListOfShoppingListsActivity.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/view/ListOfShoppingListsActivity.java
@@ -14,8 +14,8 @@ public class ListOfShoppingListsActivity extends BaseActivity {
 
         Intent intent = getIntent();
         String dataString = intent.getDataString();
+        int id = -1;
         if(dataString != null) {
-            int id = -1;
             for (int i = 0; i < dataString.length(); i++) {
                 if (dataString.substring(i, i + 5).equals("/#id=")) {
                     String idString = dataString.substring(i + 5);
@@ -26,6 +26,10 @@ public class ListOfShoppingListsActivity extends BaseActivity {
         }
 
         if (savedInstanceState == null) {
+            if(id != -1){
+                android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction().add(R.id.content_frame, ShoppingListFragment.newInstance(id)).commit();
+            }
             android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
             fragmentManager.beginTransaction().add(R.id.content_frame, ListOfShoppingListsFragment.newInstance()).commit();
         }

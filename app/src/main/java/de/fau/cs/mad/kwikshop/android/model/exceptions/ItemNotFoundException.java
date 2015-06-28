0d4adcb9087679062eb2e3a1fef780b0bc1f4d65
Andreas Grünwald <andreasgruenwald@live.de>
2015-06-28 16:44:47 +0200

    Started implementing ShoppingListManager

---
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/model/exceptions/ItemNotFoundException.java
@@ -0,0 +1,9 @@
+package de.fau.cs.mad.kwikshop.android.model.exceptions;
+
+public class ItemNotFoundException extends RuntimeException {
+
+    public ItemNotFoundException(String message) {
+        super(message);
+    }
+
+}
---
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/model/exceptions/ListNotFoundException.java
@@ -0,0 +1,9 @@
+package de.fau.cs.mad.kwikshop.android.model.exceptions;
+
+public class ListNotFoundException extends RuntimeException {
+
+    public ListNotFoundException(String message) {
+        super(message);
+    }
+
+}

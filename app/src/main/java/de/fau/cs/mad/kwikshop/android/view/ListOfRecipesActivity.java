b41b1748c43ac6d05a820c6ce6822b305d3c924c
nash <Maxx1991@hotmail.de>
2015-08-05 08:50:14 +0200

    removed recipelists from overflow menu when in ListOfRecipesActivity

--- app/src/main/java/de/fau/cs/mad/kwikshop/android/view/ListOfRecipesActivity.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/view/ListOfRecipesActivity.java
@@ -2,6 +2,8 @@ package de.fau.cs.mad.kwikshop.android.view;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import de.fau.cs.mad.kwikshop.android.R;
 
@@ -16,4 +18,14 @@ public class ListOfRecipesActivity extends BaseActivity {
             fragmentManager.beginTransaction().add(R.id.content_frame, ListOfRecipesFragment.newInstance()).commit();
         }
     }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        MenuItem recipeLists = menu.findItem(R.id.action_listofrecipes);
+        recipeLists.setVisible(false);
+
+        return true;
+    }
+
 }

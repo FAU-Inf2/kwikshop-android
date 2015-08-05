e30d9d5811cf5a30b426749791df81c2e1bc0b98
nash <Maxx1991@hotmail.de>
2015-08-05 13:08:51 +0200

    confirmation dialog only appears if "do not show.." was not set before (gets saved via shared preferences), renamed string itemdeleted with value "Item bought" to itembought

--- app/src/main/java/de/fau/cs/mad/kwikshop/android/util/SharedPreferencesHelper.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/util/SharedPreferencesHelper.java
@@ -14,6 +14,7 @@ public class SharedPreferencesHelper {
     public static final String LOCALE = "locale";
     public static final String API_ENDPOINT = "API_ENDPOINT";
     public static final String LOCATION_PERMISSION = "location_permission";
+    public static final String ITEM_DELETION_SHOW_AGAIN_MSG = "ITEM_DELETION_SHOW_AGAIN_MSG";
 
     private static final String sharedPreferencesName = "settings";
 

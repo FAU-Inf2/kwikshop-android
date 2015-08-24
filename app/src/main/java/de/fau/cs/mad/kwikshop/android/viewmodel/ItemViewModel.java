20af07b5dac369932c84b0793e8209c24ea86dc0
oq22eval <pansardinicolas@gmail.com>
2015-08-24 18:44:44 +0200

    Added ItemViewModel

--- app/src/main/java/de/fau/cs/mad/kwikshop/android/viewmodel/ItemViewModel.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/viewmodel/ItemViewModel.java
@@ -8,18 +8,16 @@ import de.fau.cs.mad.kwikshop.android.model.interfaces.SimpleStorage;
 import de.fau.cs.mad.kwikshop.android.view.DisplayHelper;
 import de.fau.cs.mad.kwikshop.android.viewmodel.common.ViewLauncher;
 import de.fau.cs.mad.kwikshop.common.Group;
+import de.fau.cs.mad.kwikshop.common.Item;
 import de.fau.cs.mad.kwikshop.common.Unit;
 
 /**
  * Created by oq22eval on 8/24/15.
  */
-public class ItemViewModel extends ItemDetailsViewModel {
+public class ItemViewModel extends Item {
     private Context context;
     private boolean isVisible = false;
 
-    public ItemViewModel(ViewLauncher viewLauncher, SimpleStorage<Unit> unitStorage, SimpleStorage<Group> groupStorage, DisplayHelper displayHelper, AutoCompletionHelper autoCompletionHelper) {
-        super(viewLauncher, unitStorage, groupStorage, displayHelper, autoCompletionHelper);
-    }
 
     public boolean isVisible(){
         return isVisible;

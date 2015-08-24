73fe3990687ec5f5637cd7851929394123758b64
oq22eval <pansardinicolas@gmail.com>
2015-08-24 17:36:03 +0200

    Created ItemViewModel to change visibility of checkBoxes.

---
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/viewmodel/ItemViewModel.java
@@ -0,0 +1,31 @@
+package de.fau.cs.mad.kwikshop.android.viewmodel;
+
+import android.content.Context;
+
+import de.fau.cs.mad.kwikshop.android.model.ArgumentNullException;
+import de.fau.cs.mad.kwikshop.android.model.AutoCompletionHelper;
+import de.fau.cs.mad.kwikshop.android.model.interfaces.SimpleStorage;
+import de.fau.cs.mad.kwikshop.android.view.DisplayHelper;
+import de.fau.cs.mad.kwikshop.android.viewmodel.common.ViewLauncher;
+import de.fau.cs.mad.kwikshop.common.Group;
+import de.fau.cs.mad.kwikshop.common.Unit;
+
+/**
+ * Created by oq22eval on 8/24/15.
+ */
+public class ItemViewModel extends ItemDetailsViewModel {
+    private Context context;
+    private boolean isVisible = false;
+
+    public ItemViewModel(ViewLauncher viewLauncher, SimpleStorage<Unit> unitStorage, SimpleStorage<Group> groupStorage, DisplayHelper displayHelper, AutoCompletionHelper autoCompletionHelper) {
+        super(viewLauncher, unitStorage, groupStorage, displayHelper, autoCompletionHelper);
+    }
+
+    public boolean isVisible(){
+        return isVisible;
+    }
+
+    public void setVisible(boolean isVisible){
+        this.isVisible = isVisible;
+    }
+}

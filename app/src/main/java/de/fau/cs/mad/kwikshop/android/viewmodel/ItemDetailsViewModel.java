2055dc4e3a41b63a9b9c12ba78ede624d239f6e9
Nicolas <pansardinicolas@gmail.com>
2015-09-01 09:45:53 +0200

    Created portuguese plurals

--- app/src/main/java/de/fau/cs/mad/kwikshop/android/viewmodel/ItemDetailsViewModel.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/viewmodel/ItemDetailsViewModel.java
@@ -201,6 +201,14 @@ public class ItemDetailsViewModel{
         return unitNames;
     }
 
+    public ArrayList<String> getSingularUnitNames(){
+        ArrayList<String> unitNames = new ArrayList<>();
+        for (Unit u : units){
+            u.getId();
+
+        }
+    }
+
     public Unit getSelectedUnit(){
         Unit selectedUnit = isNewItem() || item.getUnit() == null
                 ? unitStorage.getDefaultValue()

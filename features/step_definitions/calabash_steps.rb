ce619161943d2d99dded6cbd698f4b73a1941aaa
Andreas Gruenwald <andreasgruenwald@live.de>
2015-09-04 19:46:57 +0200

    Implemented some calabash tests for the navigation drawer and tests for creation and deletion of a shopping list

--- features/step_definitions/calabash_steps.rb
+++ features/step_definitions/calabash_steps.rb
@@ -15,3 +15,25 @@ Then(/^"(.*?)" radio button should not be selected$/) do |arg1|
   	fail("The radio button with text #{arg1} should not be selected")
   end
 end
+
+
+Then /I wait for the "([^\"]*)" dialog to close/ do |text|
+  # See if the dialog exists right now...
+  unless query( "textview text:'#{text}'" ).length == 0
+    # If it does, then wait for it to close...
+    wait_for( timeout: 60 ) { 0 == query( "textview text:'#{text}'" ).length }
+  end
+end
+
+Given /^I skipped the login$/ do
+    macro 'I press view with id "login_skip_button"'
+		macro 'I should see "Are you sure"'
+		macro 'I press "OK"'
+end
+
+Given /^I opened the navigation drawer$/ do
+    macro 'I press "Navigate up"'	
+    macro 'I wait for the view with id "drawer_layout" to appear'	
+end
+
+

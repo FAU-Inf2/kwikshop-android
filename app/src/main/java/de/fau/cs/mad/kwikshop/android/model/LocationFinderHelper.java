bfab867281adeee6b4dec5ce216b70c4d81c0041
Robert <robert.miller@fau.de>
2015-09-10 20:58:57 +0200

    remove getLocation in onProviderEnabled.

--- app/src/main/java/de/fau/cs/mad/kwikshop/android/model/LocationFinderHelper.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/model/LocationFinderHelper.java
@@ -75,6 +75,8 @@ public class LocationFinderHelper implements LocationListener {
 
     public Location getLocation() {
 
+        Log.e("LocationFinderHelper","getLocation()");
+
         double networkLat = 0, networkLong = 0, networkAcc = Double.MAX_VALUE, gpsLat = 0, gpsLong = 0, gpsAcc = Double.MAX_VALUE;
 
         LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
@@ -192,84 +194,6 @@ public class LocationFinderHelper implements LocationListener {
         return "";
     }
 
-
-    @Override
-    public void onLocationChanged(Location location) {
-        if (isBetterLocation(location, this.location))
-            this.location = location;
-    }
-
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        getLocation();
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        getLocation();
-    }
-
-    private static final int TWO_MINUTES = 1000 * 60 * 2;
-
-    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
-        if (currentBestLocation == null) {
-            // A new location is always better than no location
-            return true;
-        }
-
-        // Check whether the new location fix is newer or older
-        long timeDelta = location.getTime() - currentBestLocation.getTime();
-        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
-        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
-        boolean isNewer = timeDelta > 0;
-
-        // If it's been more than two minutes since the current location, use the new location
-        // because the user has likely moved
-        if (isSignificantlyNewer) {
-            return true;
-            // If the new location is more than two minutes older, it must be worse
-        } else if (isSignificantlyOlder) {
-            return false;
-        }
-
-        // Check whether the new location fix is more or less accurate
-        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
-        boolean isLessAccurate = accuracyDelta > 0;
-        boolean isMoreAccurate = accuracyDelta < 0;
-        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
-
-        // Check if the old and new location are from the same provider
-        boolean isFromSameProvider = isSameProvider(location.getProvider(),
-                currentBestLocation.getProvider());
-
-        // Determine location quality using a combination of timeliness and accuracy
-        if (isMoreAccurate) {
-            return true;
-        } else if (isNewer && !isLessAccurate) {
-            return true;
-        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Checks whether two providers are the same
-     */
-    private boolean isSameProvider(String provider1, String provider2) {
-        if (provider1 == null) {
-            return provider2 == null;
-        }
-        return provider1.equals(provider2);
-    }
-
-
     public static Place findClickedPlace(Marker marker, List<Place> places) {
         for (Place place : places) {
             if (place.getLatitude() - marker.getPosition().latitude == 0.0 && place.getLongitude() - marker.getPosition().longitude == 0.0) {
@@ -327,4 +251,23 @@ public class LocationFinderHelper implements LocationListener {
         return true;
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }

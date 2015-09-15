0095331894e5ad0ef5e52c01e83d7260b675d713
nash <Maxx1991@hotmail.de>
2015-09-15 10:01:32 +0200

    call super.onBackPressed() in ShoppingListActivity before eventually returning in onBackPressed

--- app/src/main/java/de/fau/cs/mad/kwikshop/android/view/ShoppingListActivity.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/view/ShoppingListActivity.java
@@ -253,6 +253,8 @@ public class ShoppingListActivity extends BaseActivity implements EditModeActivi
     @Override
     public void onBackPressed() {
 
+        super.onBackPressed();
+
         // check for barcode scanner fragment
         android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
         Fragment BarcodeScannerFragment = fragmentManager.findFragmentByTag("BARCODE_SCANNER_FRAGMENT");
@@ -266,8 +268,6 @@ public class ShoppingListActivity extends BaseActivity implements EditModeActivi
             viewModel.showDialogLeaveShoppingMode(listId);
             return;
         }
-
-        super.onBackPressed();
     }
 
 

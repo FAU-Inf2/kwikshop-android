67e0ed5ae5f32a5af2777618132f804199fc1de0
nash <Maxx1991@hotmail.de>
2015-09-16 11:43:00 +0200

    return -1 as id when getItemId gets called with a position out of range for the items ArrayList

--- app/src/main/java/de/fau/cs/mad/kwikshop/android/view/ShoppingListAdapter.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/view/ShoppingListAdapter.java
@@ -82,9 +82,14 @@ public class ShoppingListAdapter extends com.nhaarman.listviewanimations.ArrayAd
 
     @Override
     public long getItemId(int position) {
-        return items.get(position).getItem().getId();
-    }
 
+        //TODO: This still leads to unintended behaviour, but it prevents the app from crashing
+        //This is just a hotfix so this is not a release blocker anymore
+        if(position >= items.size()) return -1;
+
+        return getItem(position).getItem().getId();
+
+    }
 
     @Override
     public View getView(final int position, View view, ViewGroup parent) {

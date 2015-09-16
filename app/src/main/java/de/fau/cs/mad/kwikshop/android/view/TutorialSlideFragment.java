81fe938b8cb94d491047dbc1d698781cd5ca76fd
Robert <robert.miller@fau.de>
2015-09-16 17:36:52 +0200

    tested tutorial view pager with test images
    Merge branch 'master' of https://github.com/FAU-Inf2/kwikshop-android
    
    Conflicts:
    	app/src/main/java/de/fau/cs/mad/kwikshop/android/view/ItemDetailsFragment.java

--- app/src/main/java/de/fau/cs/mad/kwikshop/android/view/TutorialSlideFragment.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/view/TutorialSlideFragment.java
@@ -1,4 +1,45 @@
 package de.fau.cs.mad.kwikshop.android.view;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class TutorialSlideFragment {
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import de.fau.cs.mad.kwikshop.android.R;
+
+
+public class TutorialSlideFragment extends Fragment {
+
+    @InjectView(R.id.image_view_tutorial)
+    ImageView imageView;
+
+    int imageResourceId;
+
+    public TutorialSlideFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = getArguments();
+        imageResourceId = bundle.getInt(TutorialSlidesFragmentAdapter.IMAGE_RESOURCE_ID);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_tutorial_slide, container, false);
+        ButterKnife.inject(this, rootView);
+
+        imageView.setImageResource(imageResourceId);
+
+        return rootView;
+    }
+
+
 }
--- app/src/main/java/de/fau/cs/mad/kwikshop/android/view/TutorialSlidesFragmentAdapter.java
+++ app/src/main/java/de/fau/cs/mad/kwikshop/android/view/TutorialSlidesFragmentAdapter.java
@@ -1,5 +1,58 @@
 package de.fau.cs.mad.kwikshop.android.view;
 
 
-public class TutorialSlidesFragmentAdapter {
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.viewpagerindicator.IconPagerAdapter;
+
+import de.fau.cs.mad.kwikshop.android.R;
+
+public class TutorialSlidesFragmentAdapter extends FragmentPagerAdapter implements
+        IconPagerAdapter {
+
+    public static final String IMAGE_RESOURCE_ID = "image_resource";
+
+    private int[] Images = new int[] { R.drawable.ic_launcher, R.drawable.ic_about,
+            R.drawable.ic_action, R.drawable.ic_add_to_cart
+
+    };
+
+    protected static final int[] ICONS = new int[] { R.drawable.ic_cart_out,
+            R.drawable.ic_cart_out, R.drawable.ic_cart_out, R.drawable.ic_cart_out };
+
+    private int mCount = Images.length;
+
+    public TutorialSlidesFragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Bundle args = new Bundle();
+        args.putInt(IMAGE_RESOURCE_ID, Images[position]);
+        Fragment fragment = new TutorialSlideFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return mCount;
+    }
+
+    @Override
+    public int getIconResId(int index) {
+        return ICONS[index % ICONS.length];
+    }
+
+    public void setCount(int count) {
+        if (count > 0 && count <= 10) {
+            mCount = count;
+            notifyDataSetChanged();
+        }
+    }
 }
